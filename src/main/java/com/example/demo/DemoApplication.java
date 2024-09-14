c055c00091b9dcc1eb88149b692cea8ebd18ac06
wubaorui <wubaorui@bilibili.com>
2024-09-14 16:49:54 +0800

    refine

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -22,90 +22,101 @@ import java.util.*;
 public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-        new Thread(DemoApplication::extracted).start();
+        Thread thread = new Thread(DemoApplication::extracted);
+        thread.start();
     }
 
     private static void extracted() {
-        // iphone
-        String url = "https://www.apple.com.cn/shop/pickup-message-recommendations?mts.0=regular&mts.1=compact&searchNearby=true&store=R683&product=MU2Q3CH/A";
-        // watch
+        try {
+            // iphone
+            String url = "https://www.apple.com.cn/shop/pickup-message-recommendations?mts.0=regular&mts.1=compact&cppart=UNLOCKED/WW&searchNearby=true&store=R683&product=MYTM3CH/A";
+            // watch
 //        String url = "https://www.apple.com.cn/shop/fulfillment-messages?searchNearby=true&parts.0=MQFG3CH/A&option.0=MQG03CH/A,MQEP3FE/A&store=R581";
-        String webhooks = "https://open.feishu.cn/open-apis/bot/v2/hook/c9e3dd52-57af-4ae8-9f4c-da737894c212";
+            String webhooks = "https://open.feishu.cn/open-apis/bot/v2/hook/269714ca-790b-4f6e-8c4e-0eb8dcad85a1";
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        byte[] preBytes = new byte[0];
-        while (true) {
-            HttpRequest request;
-            try {
-                request = HttpRequest.newBuilder().uri(new URI(url))
-                        .GET().timeout(Duration.ofSeconds(5)).build();
-            } catch (URISyntaxException e) {
-                continue;
-            }
-            HttpClient client = HttpClient.newHttpClient();
-            HttpResponse<String> response;
-            try {
-                response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            } catch (IOException | InterruptedException e) {
-                continue;
-            }
-            System.out.println("[" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")) + "]" + response.body());
-            Map<String, Object> recommend;
-            try {
-                recommend = objectMapper.readValue(response.body(), new TypeReference<>() {
-                });
-            } catch (JsonProcessingException e) {
-                continue;
-            }
-
-
-            List<Map<String, Object>> res = new ArrayList<>();
-
-            ((List<Map<String, Object>>) ((Map<String, Object>) ((Map<String, Object>) recommend.get("body")).get("PickupMessage")).get("stores"))
-                    .stream()
-                    .filter(x -> !CollectionUtils.isEmpty(x))
-                    .map(x -> (Map<String, Object>) x.get("partsAvailability"))
-                    .filter(x -> !CollectionUtils.isEmpty(x))
-                    .map(Map::values)
-                    .flatMap(x -> x.stream().map(z -> (Map<String, Object>) z).filter(z -> Objects.nonNull(z.get("partNumber")) && z.get("partNumber").toString().startsWith("MU")).map(y -> (Map<String, Object>) ((Map<String, Object>) y.get("messageTypes")).get("regular")))
-                    .forEach(x -> {
-                        Map<String, Object> map = new HashMap<>();
-                        map.put("时间", x.get("storePickupQuote"));
-                        map.put("型号", x.get("storePickupProductTitle"));
-                        res.add(map);
-                    });
-
-
-            if (!CollectionUtils.isEmpty(res)) {
-                System.out.println("======找到啦" + res + "找到啦======");
-                Feishu content = new Feishu("text", new Content(res.toString()));
-                byte[] bytes;
+            ObjectMapper objectMapper = new ObjectMapper();
+            byte[] preBytes = new byte[0];
+            while (true) {
+                HttpRequest request;
                 try {
-                    bytes = objectMapper.writeValueAsBytes(content);
-                } catch (JsonProcessingException e) {
-                    continue;
-                }
-                if (Arrays.equals(preBytes, bytes)) {
-                    continue;
-                }
-                preBytes = bytes;
-                try {
-                    request = HttpRequest.newBuilder().uri(new URI(webhooks)).POST(HttpRequest.BodyPublishers.ofByteArray(bytes)).build();
+                    request = HttpRequest.newBuilder().uri(new URI(url))
+                            .GET().timeout(Duration.ofSeconds(5)).build();
                 } catch (URISyntaxException e) {
                     continue;
                 }
+                HttpClient client = HttpClient.newHttpClient();
+                HttpResponse<String> response;
                 try {
                     response = client.send(request, HttpResponse.BodyHandlers.ofString());
                 } catch (IOException | InterruptedException e) {
                     continue;
                 }
-                System.out.println("Feishu response" + response);
+                System.out.println("[" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")) + "]" + response.body());
+                Map<String, Object> recommend;
+                try {
+                    recommend = objectMapper.readValue(response.body(), new TypeReference<>() {
+                    });
+                } catch (JsonProcessingException e) {
+                    continue;
+                }
+
+
+                List<Map<String, Object>> res = new ArrayList<>();
+
+                ((List<Map<String, Object>>) ((Map<String, Object>) ((Map<String, Object>) recommend.get("body")).get("PickupMessage")).get("stores"))
+                        .stream()
+                        .filter(x -> !CollectionUtils.isEmpty(x))
+                        .map(x -> (Map<String, Object>) x.get("partsAvailability"))
+                        .filter(x -> !CollectionUtils.isEmpty(x))
+                        .map(Map::values)
+                        .flatMap(x -> x.stream().map(z -> (Map<String, Object>) z)
+                                .filter(z -> {
+                                    Object partNumber = z.get("partNumber");
+                                    return Objects.nonNull(partNumber) && partNumber.toString().startsWith("MYT");
+                                })
+                                .map(y -> (Map<String, Object>) ((Map<String, Object>) y.get("messageTypes")).get("regular"))
+                        )
+                        .forEach(x -> {
+                            Map<String, Object> map = new HashMap<>();
+                            map.put("时间", x.get("storePickupQuote"));
+                            map.put("型号", x.get("storePickupProductTitle"));
+                            res.add(map);
+                        });
+
+
+                if (!CollectionUtils.isEmpty(res)) {
+                    System.out.println("======找到啦" + res + "找到啦======");
+                    Feishu content = new Feishu("text", new Content(res.toString()));
+                    byte[] bytes;
+                    try {
+                        bytes = objectMapper.writeValueAsBytes(content);
+                    } catch (JsonProcessingException e) {
+                        continue;
+                    }
+                    if (Arrays.equals(preBytes, bytes)) {
+                        continue;
+                    }
+                    preBytes = bytes;
+                    try {
+                        request = HttpRequest.newBuilder().uri(new URI(webhooks)).POST(HttpRequest.BodyPublishers.ofByteArray(bytes)).build();
+                    } catch (URISyntaxException e) {
+                        continue;
+                    }
+                    try {
+                        response = client.send(request, HttpResponse.BodyHandlers.ofString());
+                    } catch (IOException | InterruptedException e) {
+                        continue;
+                    }
+                    System.out.println("Feishu response" + response);
+                }
+                try {
+                    Thread.sleep(500 + new Random().nextInt(200));
+                } catch (InterruptedException e) {
+                    continue;
+                }
             }
-            try {
-                Thread.sleep(500 + new Random().nextInt(200));
-            } catch (InterruptedException e) {
-                continue;
-            }
+        } catch (Exception e) {
+
         }
     }
 
